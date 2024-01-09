6c22ee186698108074d6c7625d8d46d7e695b367
RavaszJan <janravasz@gmail.com>
2024-01-09 21:12:04 +0100

    ooc2 dedenie

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 class Person:
-    def __init__(self,name,age,gender,occupation,color):
+    def __init__(self,name,age,gender,occupation):
         self.name=name
         self.age=age
         self.gender=gender
-        self__occupation=occupation
-        self.color=color
+        self._occupation=occupation
+
 
     def pozdrav(self):
-        print(f"Ahoj volam sa {self.name} a mam {self.age} rokov a farbu oci {self.color}")
+        print(f"Ahoj volam sa {self.name} a mam {self.age} rokov ")
 
     def miesto(self):
-        print(f"{self.name} ma bydlisko {self.occupation}")
+        print(f"{self.name} ma bydlisko {self._occupation}")
     def vek_plus(self,a):
         b=self.age+a
         print("Moj pridany vek je",b)
@@ -18,16 +18,27 @@ class Person:
         print(f"Moj skutocny vek je {self.age}")
 
 
-patrik=Person("Patrik",30,"muz","Bratislava","modra")
-patrik.pozdrav()
+class Student(Person):
+    def __init__(self,name,age,gender,_occupation,score):
+        super().__init__(name,age,gender,_occupation)
+        self.score=score
+
+    def jeGenius(self):
+        if self.score>90:
+            print(f"{self.name} je genius a {self.gender}")
+        else:
+            print(f"{self.name} nie je genius a {self.gender}")
+
+patrik=Student("Patrik",30,"muz","Bratislava",70)
+patrik.jeGenius()
 
 
-jan=Person("Jan",48,"muz","Bratislava","modra")
+
+jan=Person("Jan",48,"muz","Bratislava")
 jan.name="peter"
 print(jan.name)
-jan.pozdrav()
+jan.miesto()
 jan.vek_plus(10)
 
 jan.vek()
-print(jan.color)
 
